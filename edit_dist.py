c29a2d828e7122859c5c54c23a498065949aaa55
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-12-06 14:08:10 +0530

    Edit distance calculation

--- edit_dist.py
+++ edit_dist.py
@@ -1,1 +1,21 @@
 # calculating edit distance between two strings
+def get_edit_distance(string1, string2):
+    length1 = len(string1)
+    length2 = len(string2)
+
+    arr = [[0 for _ in range(length1 + 1)] for _ in range(2)]
+    for i in range(0, length1 + 1):
+        arr[0][i] = i
+    for i in range(1, length2 + 1):
+        for j in range(0, length1 + 1):
+            if (j == 0):
+                arr[i % 2][j] = i
+
+            elif (string1[j - 1] == string2[i - 1]):
+                arr[i % 2][j] = arr[(i - 1) % 2][j - 1]
+            else:
+                mini = min(arr[i % 2][j - 1],arr[(i - 1) % 2][j - 1])
+                arr[i % 2][j] = (1 + min(arr[(i - 1) % 2][j],mini))
+
+    return arr[length2 % 2][length1]
+
