c58e69a84735808bb3da4c3a93fbead6f0025aca
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-12-06 14:25:17 +0530

    String and substring generation

--- string_gen.py
+++ string_gen.py
@@ -1,1 +1,23 @@
 # generating 20 random strings of 600 characters
+
+import random
+from itertools import combinations
+
+SIZE=600
+ALPHABET_SET = ["A", "C", "G", "T"]
+SUBSTRING_LEN=15
+NUM = 20
+def gen_str_and_substrings():
+    strings = []
+    substrings = []
+    input_file = open("inputs.txt", "w")
+    for i in range(NUM):
+        random_string = ''.join(random.choices(ALPHABET_SET, k = SIZE))
+        strings.append(random_string)
+        substring = [random_string[x:y] for x, y in combinations(range(len(random_string) + 1), r=2) if len(random_string[x:y]) == SUBSTRING_LEN]
+
+        input_file.write(random_string + "\n")
+        substrings.append(substring)
+    input_file.close()
+
+    return strings, substrings
